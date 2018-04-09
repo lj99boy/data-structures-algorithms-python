1d1cf01aa6a015c5cb1f925f34f583bacc3650fd
milu <lj99boy@163.com>
2018-04-09 15:12:43 +0800

    update gitignore

---
+++ linear_table/sequence_table.py
@@ -0,0 +1,145 @@
+"""
+ a sequence-table base on python-list
+"""
+
+class SequenceTableValueError(ValueError):
+    pass
+
+
+class SequenceTable:
+
+    def __init__(self):
+        self._max_length = 3
+        self._num = 0
+        self._store = []
+
+    @property
+    def store(self):
+        return self._store
+
+    @property
+    def num(self):
+        return self._num
+
+    @property
+    def max_length(self):
+        return self._max_length
+
+    def is_empty(self):
+        if self._num == 0:
+            return True
+
+    def len(self):
+        return self._num
+
+    def extend_store(self):
+        self._max_length = self._max_length * 2
+        new_store = []
+        index = 0
+        for i in self._store:
+            new_store.insert(index,i)
+            index += 1
+        self._store = new_store
+
+    def prepend(self,elem):
+        if self._num == self._max_length:
+            self.extend_store()
+        end_tag = self._num
+        if(end_tag == 0):
+            self._store.insert(0,elem)
+        else:
+            for item in self._store[::-1]:
+                if (end_tag == self._num):
+                    self._store.insert(end_tag, item)
+                else:
+                    self._store[end_tag] = item
+                end_tag -= 1
+            self._store[0] = elem
+        self._num += 1
+
+    def append(self,elem):
+        if self._num == self._max_length:
+            self.extend_store()
+        self._store.insert(self._num,elem)
+        self._num += 1
+
+    def insert(self,elem,i):
+        if self._num == self._max_length:
+            self.extend_store()
+
+        if(i<0 or i>self._num):
+            raise SequenceTableValueError('out of range')
+        elif(i==0):
+            self.prepend(elem)
+        elif(i==self._num):
+            self.append(elem)
+        else:
+            end_tag = self._num
+            for item in self._store[:i-1:-1]:
+                if(end_tag == self._num):
+                    self._store.insert(end_tag,item)
+                else:
+                    self._store[end_tag] = item
+                end_tag -=1
+            self._store[i] = elem
+            self._num += 1
+
+    def del_first(self):
+        start_tag = 0
+        for item in self._store[1:]:
+            self._store[start_tag] = item
+            start_tag += 1
+        del self._store[self._num-1]
+        self._num -= 1
+
+    def del_last(self):
+        del self._store[self._num-1]
+        self._num -= 1
+
+    def del_item(self,i):
+        if(i<0 or i>self._num-1):
+            raise SequenceTableValueError('out of range')
+        elif(i == 0):
+            self.del_first()
+        elif(i == self._num-1):
+            self.del_last()
+        else:
+            start_tag = i
+            for item in self._store[i+1:]:
+                self._store[start_tag] = item
+                start_tag += 1
+            del self._store[self._num-1]
+            self._num -= 1
+
+
+    def search(self):
+        pass
+
+    def __str__(self):
+        return ",".join(str(self._store))
+
+    
+
+s1 = SequenceTable()
+print(s1.num)
+print(s1.max_length)
+
+
+s1.prepend(1)
+s1.prepend(2)
+s1.prepend(3)
+s1.prepend(4)
+s1.append(9)
+# s1.append(2)
+# s1.append(3)
+# s1.append(4)
+s1.insert(7,1)
+s1.del_item(4)
+# s1.del_first()
+# s1.del_last()
+# s1.insert(7,1)
+# print(s1.store[:2:-1])
+
+print(s1.store)
+print(s1.num)
+print(s1.max_length)
--- samples/chapter2_class/app.py
+++ samples/chapter2_class/app.py
@@ -73,3 +73,92 @@ class Person:
         )
 
 
+
+class Student(Person):
+    _id_num = 0
+
+    @classmethod
+    def _id_gen(cls):
+        cls._id_num += 1
+        year = datetime.date.today().year
+        return "1{:04}{:05}".format(year,cls._id_num)
+
+    def __init__(self,name,sex,birthday,department):
+        super().__init__(
+            name,
+            sex,
+            birthday,
+            Student._id_gen()
+        )
+        self._department = department
+        self._enroll_date = datetime.date.today()
+        self._courses = {}
+
+    @property
+    def scores(self):
+        return [(cname,self._courses[cname]) for cname in self._courses]
+
+    def set_score(self,course_name,score):
+        if course_name not in self._courses:
+            raise PersonValueError("No this course selected:",course_name)
+
+        self._courses[course_name] = score
+
+    def set_course(self,course_name):
+        self._courses[course_name] = None
+
+    def details(self):
+        return ", ".join((super().details(),
+                         'enroll_date: '+str(self._enroll_date),
+                         'department: '+self._department,
+                         'courses: '+ str(self.scores)
+                         ))
+
+# s1 = Student('s1','female',(1990,10,1),'miao')
+# print(s1)
+# print(s1.details())
+
+
+class Staff(Person):
+    _id_num = 0
+
+    @classmethod
+    def _id_gen(cls,birthday):
+        cls._id_num +=1
+        birth_year = datetime.date(*birthday).year
+        return "0{:04}{:05}".format(birth_year,cls._id_num)
+
+    def __init__(self,name,sex,birthday,entry_date=None):
+        super().__init__(name,sex,birthday,self._id_gen(birthday))
+
+        if entry_date:
+            try:
+                self._entry_date = datetime.date(*entry_date)
+            except:
+                raise PersonValueError("Wrong date:",entry_date)
+        else:
+            self._entry_date = datetime.date.today()
+
+        self._salary = 1720
+        self._department = "unknown"
+        self._position = "unknown"
+
+    def set_salary(self,amount):
+        if not type(amount) is int:
+            raise TypeError
+        self._salary = amount
+
+    def set_position(self,position):
+        self._position = position
+
+    def set_department(self,department):
+        self._department = department
+
+    def details(self):
+        return ", ".join((super().details(),
+                         'entry_date: '+str(self._entry_date),
+                         'department: '+self._department,
+                         'position: '+ str(self._position),
+                         'salary: '+ str(self._salary),
+                         ))
+
