5b4a7ea9b5ba2b0d7d06e5018f1859ab591257a7
kyou_virtual_machine <lj99boy@hotmail.com>
2018-04-22 00:19:49 +0800

    f

--- problem/maze/stack_backtrace.py
+++ problem/maze/stack_backtrace.py
@@ -1,3 +1,5 @@
+from stack_and_queue.stack import SStack
+
 def mark(maze,pos):
     maze[pos[0],pos[1]] = 2
 
@@ -20,3 +22,28 @@ def find_path(maze,pos,end):
                 return True
     return False
 
+
+# using backtrace
+def maze_solver(maze,start,end):
+    if start == end:
+        print(start)
+        return
+
+    st = SStack()
+    mark(maze,start)
+    st.push((start,0))
+    while not st.is_empty():
+        pos , nxt = st.pop()
+        for i in range(nxt,4):
+            nextp = (pos[0]+dirs[i][0],
+                     pos[1]+dirs[i][1]
+                     )
+            if nextp == end:
+                # print_path(end,pos,st)
+                return
+            if passable(maze,nextp):
+                st.push((pos,i+1))
+                mark(maze,nextp)
+                st.push((nextp,0))
+                break
+    print("no path found")
